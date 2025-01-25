9943ad901d598f5cdc675031403f498e57e28eb8
Rena Kunisaki <i.am.inuyasha@gmail.com>
2025-01-25 17:28:28 -0500

    match pi.c except some mystey padding

--- include/Map.h
+++ include/Map.h
@@ -1,119 +1,120 @@
-#define MapId_frontend 0
-#define MapId_frontend2 1
-#define MapId_dragrock 2
-#define MapId_krazoapalace 3
-#define MapId_temple 4
-#define MapId_hightop 5
-#define MapId_discovery 6
-#define MapId_hollow 7
-#define MapId_hollow2 8
-#define MapId_mazecave 9
-#define MapId_wastes 10
-#define MapId_warlock 11
-#define MapId_fortress 12
-#define MapId_wallcity 13
-#define MapId_swapcircle 14
-#define MapId_cloudtreasure 15
-#define MapId_clouddungeon 16
-#define MapId_cloudtrap 17
-#define MapId_moonpass 18
-#define MapId_snowmines 19
-#define MapId_krashrin2 20
-#define MapId_kraztest 21
-#define MapId_krazchamber 22
-#define MapId_newicemount 23
-#define MapId_newicemount2 24
-#define MapId_newicemount3 25
-#define MapId_animtest 26
-#define MapId_snowmines2 27
-#define MapId_snowmines3 28
-#define MapId_capeclaw 29
-#define MapId_insidegal 30
-#define MapId_dfshrine 31
-#define MapId_mmshrine 32
-#define MapId_ecshrine 33
-#define MapId_gpshrine 34
-#define MapId_diamondbay 35
-#define MapId_earthwalker 36
-#define MapId_willow 37
-#define MapId_arwing 38
-#define MapId_dbshrine 39
-#define MapId_nwshrine 40
-#define MapId_ccshrine 41
-#define MapId_wgshrine 42
-#define MapId_cloudrace 43
-#define MapId_finalboss 44
-#define MapId_wminsert 45
-#define MapId_snowmines4 46
-#define MapId_snowmines5 47
-#define MapId_trexboss 48
-#define MapId_mikelava 49
-#define MapId_dfptop 50
-#define MapId_swapstore 51
-#define MapId_dragbot 52
-#define MapId_kamdrag 53
-#define MapId_magicave 54
-#define MapId_duster 55
-#define MapId_linkb 56
-#define MapId_cloudjoin 57
-#define MapId_arwingtoplanet 58
-#define MapId_arwingdarkice 59
-#define MapId_arwingcloud 60
-#define MapId_arwingcity 61
-#define MapId_arwingdragon 62
-#define MapId_gamefront 63
-#define MapId_linklevel 64
-#define MapId_greatfox 65
-#define MapId_linka 66
-#define MapId_linkc 67
-#define MapId_linkd 68
-#define MapId_linke 69
-#define MapId_linkf 70
-#define MapId_linkg 71
-#define MapId_linkh 72
-#define MapId_linkj 73
-#define MapId_linki 74
-#define MapId_dfpodium 75
-#define MapId_dfcradle 76
-#define MapId_dfcavehatch1 77
-#define MapId_dfcavehatch2 78
-#define MapId_scstatue 79
-#define MapId_galleonship 80
-#define MapId_cfgalleon 81
-#define MapId_cfgangplank 82
-#define MapId_nwtreebridge 83
-#define MapId_cfdungeonblock 84
-#define MapId_cloudrunnermap 85
-#define MapId_ccbridge 86
-#define MapId_cfcolumn 87
-#define MapId_nwboulder 88
-#define MapId_cfprisondoor 89
-#define MapId_cfprisoncage 90
-#define MapId_nwtreebridge2 91
-#define MapId_dim2iceblock1 92
-#define MapId_dimpushblock 93
-#define MapId_dim2iceblock2 94
-#define MapId_dimhornplinth 95
-#define MapId_nwshcolpush 96
-#define MapId_dim2lift 97
-#define MapId_dim2icefloe 98
-#define MapId_dim2icefloe1 99
-#define MapId_dim2icefloe2 100
-#define MapId_cfliftplat 101
-#define MapId_imspacecraft 102
-#define MapId_dimbossgut 103
-#define MapId_wmcolrise 104
-#define MapId_vfpslide1 105
-#define MapId_vfpslide2 106
-#define MapId_drpushcart 107
-#define MapId_drliftplat 108
-#define MapId_dim2stonepillar 109
-#define MapId_bossdrakorflatr 110
-#define MapId_wcbouncycrate 111
-#define MapId_wcpushblock 112
-#define MapId_wctemplelift 113
-#define MapId_KamColumn 114
-#define MapId_dbstepstone 115
-#define MapId_vfppushblock 116
-#define MapId_None -1
-
+typedef enum {
+    MapId_frontend = 0,
+    MapId_frontend2 = 1,
+    MapId_dragrock = 2,
+    MapId_krazoapalace = 3,
+    MapId_temple = 4,
+    MapId_hightop = 5,
+    MapId_discovery = 6,
+    MapId_hollow = 7,
+    MapId_hollow2 = 8,
+    MapId_mazecave = 9,
+    MapId_wastes = 10,
+    MapId_warlock = 11,
+    MapId_fortress = 12,
+    MapId_wallcity = 13,
+    MapId_swapcircle = 14,
+    MapId_cloudtreasure = 15,
+    MapId_clouddungeon = 16,
+    MapId_cloudtrap = 17,
+    MapId_moonpass = 18,
+    MapId_snowmines = 19,
+    MapId_krashrin2 = 20,
+    MapId_kraztest = 21,
+    MapId_krazchamber = 22,
+    MapId_newicemount = 23,
+    MapId_newicemount2 = 24,
+    MapId_newicemount3 = 25,
+    MapId_animtest = 26,
+    MapId_snowmines2 = 27,
+    MapId_snowmines3 = 28,
+    MapId_capeclaw = 29,
+    MapId_insidegal = 30,
+    MapId_dfshrine = 31,
+    MapId_mmshrine = 32,
+    MapId_ecshrine = 33,
+    MapId_gpshrine = 34,
+    MapId_diamondbay = 35,
+    MapId_earthwalker = 36,
+    MapId_willow = 37,
+    MapId_arwing = 38,
+    MapId_dbshrine = 39,
+    MapId_nwshrine = 40,
+    MapId_ccshrine = 41,
+    MapId_wgshrine = 42,
+    MapId_cloudrace = 43,
+    MapId_finalboss = 44,
+    MapId_wminsert = 45,
+    MapId_snowmines4 = 46,
+    MapId_snowmines5 = 47,
+    MapId_trexboss = 48,
+    MapId_mikelava = 49,
+    MapId_dfptop = 50,
+    MapId_swapstore = 51,
+    MapId_dragbot = 52,
+    MapId_kamdrag = 53,
+    MapId_magicave = 54,
+    MapId_duster = 55,
+    MapId_linkb = 56,
+    MapId_cloudjoin = 57,
+    MapId_arwingtoplanet = 58,
+    MapId_arwingdarkice = 59,
+    MapId_arwingcloud = 60,
+    MapId_arwingcity = 61,
+    MapId_arwingdragon = 62,
+    MapId_gamefront = 63,
+    MapId_linklevel = 64,
+    MapId_greatfox = 65,
+    MapId_linka = 66,
+    MapId_linkc = 67,
+    MapId_linkd = 68,
+    MapId_linke = 69,
+    MapId_linkf = 70,
+    MapId_linkg = 71,
+    MapId_linkh = 72,
+    MapId_linkj = 73,
+    MapId_linki = 74,
+    MapId_dfpodium = 75,
+    MapId_dfcradle = 76,
+    MapId_dfcavehatch1 = 77,
+    MapId_dfcavehatch2 = 78,
+    MapId_scstatue = 79,
+    MapId_galleonship = 80,
+    MapId_cfgalleon = 81,
+    MapId_cfgangplank = 82,
+    MapId_nwtreebridge = 83,
+    MapId_cfdungeonblock = 84,
+    MapId_cloudrunnermap = 85,
+    MapId_ccbridge = 86,
+    MapId_cfcolumn = 87,
+    MapId_nwboulder = 88,
+    MapId_cfprisondoor = 89,
+    MapId_cfprisoncage = 90,
+    MapId_nwtreebridge2 = 91,
+    MapId_dim2iceblock1 = 92,
+    MapId_dimpushblock = 93,
+    MapId_dim2iceblock2 = 94,
+    MapId_dimhornplinth = 95,
+    MapId_nwshcolpush = 96,
+    MapId_dim2lift = 97,
+    MapId_dim2icefloe = 98,
+    MapId_dim2icefloe1 = 99,
+    MapId_dim2icefloe2 = 100,
+    MapId_cfliftplat = 101,
+    MapId_imspacecraft = 102,
+    MapId_dimbossgut = 103,
+    MapId_wmcolrise = 104,
+    MapId_vfpslide1 = 105,
+    MapId_vfpslide2 = 106,
+    MapId_drpushcart = 107,
+    MapId_drliftplat = 108,
+    MapId_dim2stonepillar = 109,
+    MapId_bossdrakorflatr = 110,
+    MapId_wcbouncycrate = 111,
+    MapId_wcpushblock = 112,
+    MapId_wctemplelift = 113,
+    MapId_KamColumn = 114,
+    MapId_dbstepstone = 115,
+    MapId_vfppushblock = 116,
+    #define MapId_None -1
+} MapId;
--- src/main/pi.c
+++ src/main/pi.c
@@ -35,8 +35,8 @@ typedef struct {
 void* loadDataFile(DataFileId32 file, int);
 Texture* textureLoad(int id, int);
 ObjInstance* Object_objSetupObjectActual(ObjDef *objDef,
-    u8 /*objSetupObjectActual_flags*/ flags,
-    u8 /*MapId8*/ mapId,
+    objSetupObjectActual_flags flags,
+    MapId mapId,
     s32 romDefNo,
     ObjInstance *pMatrix,
     void *unused);
@@ -46,7 +46,6 @@ Animation * loadAnimation (Model * model, short id, short id2, void * dest);
 
 typedef struct {
     struct {
-        u8 unk[0x18];
         bool unk00;
         u8 type; //AssetTypeEnum
         int id;
@@ -92,85 +91,88 @@ typedef struct {
     };
 } AssetDef;
 
-AssetDef AssetDef_80352f00;
+static struct {
+    u8 unk[0x18];
+    AssetDef adef;
+} AssetDef_80352f00;
 
 void loadAsset_file(void **dest, DataFileId32 file) { //800777F0
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_FILE;
-    AssetDef_80352f00.header.id = file;
-    AssetDef_80352f00.header.dest = dest;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_FILE;
+    AssetDef_80352f00.adef.header.id = file;
+    AssetDef_80352f00.adef.header.dest = dest;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_fileWithOffset(void **dest, DataFileId32 file) { //8007784C
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_FILE_WITH_OFFSET;
-    AssetDef_80352f00.header.id = file;
-    AssetDef_80352f00.header.dest = dest;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_FILE_WITH_OFFSET;
+    AssetDef_80352f00.adef.header.id = file;
+    AssetDef_80352f00.adef.header.dest = dest;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_fileWithOffsetLength(
     void *dest,DataFileId32 file,int offset,int length) { //800778A8
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_FILE_WITH_ID_SIZE;
-    AssetDef_80352f00.header.id = file;
-    AssetDef_80352f00.header.dest = dest;
-    AssetDef_80352f00.file.offset = offset;
-    AssetDef_80352f00.file.size = length;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_FILE_WITH_ID_SIZE;
+    AssetDef_80352f00.adef.header.id = file;
+    AssetDef_80352f00.adef.header.dest = dest;
+    AssetDef_80352f00.adef.file.offset = offset;
+    AssetDef_80352f00.adef.file.size = length;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_Character(ObjInstance **result,ObjDef *def,uint flags,
 int mapId,int objNo,ObjInstance *pMatrix,void *param7) { //8007791C
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_CHARACTER;
-    AssetDef_80352f00.character.objDef = def;
-    AssetDef_80352f00.character.objFlags = flags;
-    AssetDef_80352f00.character.mapId = mapId;
-    AssetDef_80352f00.character.objNo = objNo;
-    AssetDef_80352f00.character.pMatrix = pMatrix;
-    AssetDef_80352f00.character.param7 = param7;
-    AssetDef_80352f00.header.dest = result;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_CHARACTER;
+    AssetDef_80352f00.adef.character.objDef = def;
+    AssetDef_80352f00.adef.character.objFlags = flags;
+    AssetDef_80352f00.adef.character.mapId = mapId;
+    AssetDef_80352f00.adef.character.objNo = objNo;
+    AssetDef_80352f00.adef.character.pMatrix = pMatrix;
+    AssetDef_80352f00.adef.character.param7 = param7;
+    AssetDef_80352f00.adef.header.dest = result;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_Texture(void *param1,DataFileId32 param2) { //800779B4
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_TEXTURE;
-    AssetDef_80352f00.header.id = param2;
-    AssetDef_80352f00.header.dest = param1;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_TEXTURE;
+    AssetDef_80352f00.adef.header.id = param2;
+    AssetDef_80352f00.adef.header.dest = param1;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_DLL(DLL_func **dll,u32 /*DLL_ID*/ dllId,int *param3) { //80077A10
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_DLL;
-    AssetDef_80352f00.header.id = dllId;
-    AssetDef_80352f00.header.dest = dll;
-    AssetDef_80352f00.dll.buffer = param3;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_DLL;
+    AssetDef_80352f00.adef.header.id = dllId;
+    AssetDef_80352f00.adef.header.dest = dll;
+    AssetDef_80352f00.adef.dll.buffer = param3;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_modelInstance(void *dest,DataFileId32 id,int *param3) { //80077A78
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_MODEL_INSTANCE;
-    AssetDef_80352f00.header.id = id;
-    AssetDef_80352f00.header.dest = dest;
-    AssetDef_80352f00.modelInstance.buffer = param3;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_MODEL_INSTANCE;
+    AssetDef_80352f00.adef.header.id = id;
+    AssetDef_80352f00.adef.header.dest = dest;
+    AssetDef_80352f00.adef.modelInstance.buffer = param3;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void loadAsset_Animation(int *param1,
 short param2,short param3,uint param4,Model *model) { //80077AE0
-    AssetDef_80352f00.header.unk00 = true;
-    AssetDef_80352f00.header.type = ASSET_TYPE_ANIMATION;
-    AssetDef_80352f00.header.id = param2;
-    AssetDef_80352f00.header.dest = param1; //stores return code
-    AssetDef_80352f00.anim.unk0C = param3;
-    AssetDef_80352f00.anim.objFlags = param4;
-    AssetDef_80352f00.anim.model = model;
-    loadAsset(&AssetDef_80352f00.header.unk00);
+    AssetDef_80352f00.adef.header.unk00 = true;
+    AssetDef_80352f00.adef.header.type = ASSET_TYPE_ANIMATION;
+    AssetDef_80352f00.adef.header.id = param2;
+    AssetDef_80352f00.adef.header.dest = param1; //stores return code
+    AssetDef_80352f00.adef.anim.unk0C = param3;
+    AssetDef_80352f00.adef.anim.objFlags = param4;
+    AssetDef_80352f00.adef.anim.model = model;
+    loadAsset(&AssetDef_80352f00.adef);
 }
 
 void nop_80077B60(void) { //80077B60
@@ -180,12 +182,6 @@ void nop_onUnloadMap(void) { //80077B64
 }
 
 int loadAsset(AssetDef *load) { //80077B68
-    void *pvVar1;
-    ObjInstance *pOVar2;
-    Texture *pTVar3;
-    DLL_func **ppDVar4;
-    char *pcVar5;
-    Animation *pAVar6;
     int iVar7;
 
     switch(load->header.type) {
@@ -200,7 +196,7 @@ int loadAsset(AssetDef *load) { //80077B68
 
         case ASSET_TYPE_FILE_WITH_ID_SIZE:
             loadDataFileWithLength(load->header.id,load->header.dest,
-                load->file.size,(u32)load->file.offset);
+                load->file.offset, load->file.size);
             break;
 
         case ASSET_TYPE_CHARACTER:
