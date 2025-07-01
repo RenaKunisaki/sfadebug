ea0b977c78b216f009f73587a012076d141cbd97
Rena Kunisaki <i.am.inuyasha@gmail.com>
2025-07-01 18:37:00 -0400

    add dolphin libs to configure.py

--- configure.py
+++ configure.py
@@ -164,7 +164,7 @@ config.ldflags = [
     "-nodefaults",
     "-g",
     "-sym on",
-    "-warn off", # remove 8 zillion lines about .note.split
+    "-warn off",  # remove 8 zillion lines about .note.split
     # "-listclosure", # Uncomment for Wii linkers
 ]
 if args.debug:
@@ -174,7 +174,7 @@ if args.map:
     # config.ldflags.append("-listclosure") # For Wii linkers
 # Use for any additional files that should cause a re-configure when modified
 config.reconfig_deps = [
-    'config/GSAP01-DEBUG/ldscript.tpl',
+    "config/GSAP01-DEBUG/ldscript.tpl",
 ]
 
 # Optional numeric ID for decomp.me preset
@@ -191,16 +191,16 @@ cflags_base = [
     "-fp hardware",
     "-Cpp_exceptions off",
     # "-W all",
-    #"-O4,p",
-    #"-O1,p",
+    # "-O4,p",
+    # "-O1,p",
     "-O0",
     "-opt peephole",
-    #"-opt off",
+    # "-opt off",
     "-g",
-    #"-func_align 8",
-    #"-common on",
-    #"-inline auto",
-    #"-inline noauto,deferred",
+    # "-func_align 8",
+    # "-common on",
+    # "-inline auto",
+    # "-inline noauto,deferred",
     "-use_lmw_stmw on",
     '-pragma "cats off"',
     '-pragma "warn_notinlined off"',
@@ -218,10 +218,10 @@ cflags_base = [
 ]
 
 # Debug flags
-#if args.debug:
+# if args.debug:
 #    # Or -sym dwarf-2 for Wii compilers
 #    cflags_base.extend(["-sym on", "-DDEBUG=1"])
-#else:
+# else:
 #    cflags_base.append("-DNDEBUG=1")
 
 # Metrowerks library flags
@@ -254,9 +254,12 @@ def DolphinLib(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
         "objects": objects,
     }
 
-Matching = True                   # Object matches and should be linked
-NonMatching = False               # Object does not match and should not be linked
-Equivalent = config.non_matching  # Object should be linked when configured with --non-matching
+
+Matching = True  # Object matches and should be linked
+NonMatching = False  # Object does not match and should not be linked
+Equivalent = (
+    config.non_matching
+)  # Object should be linked when configured with --non-matching
 
 
 # Object is only matching for specific versions
@@ -280,6 +283,211 @@ config.libs = [
     DolphinLib("ai", [
         Object(Matching, "dolphin/ai/ai.c"),
     ]),
+    DolphinLib("amcnotstub", [
+        Object(Matching, "dolphin/amcnotstub/amcnotstub.c"),
+    ]),
+    DolphinLib("amcstubs", [
+        Object(Matching, "dolphin/amcstubs/AmcExi2Stubs.c"),
+    ]),
+    DolphinLib("ar", [
+        Object(Matching, "dolphin/ar/ar.c"),
+        Object(Matching, "dolphin/ar/arq.c"),
+    ]),
+    DolphinLib("ax", [
+        Object(Matching, "dolphin/ax/AX.c"),
+        Object(Matching, "dolphin/ax/AXAlloc.c"),
+        Object(Matching, "dolphin/ax/AXAux.c"),
+        Object(Matching, "dolphin/ax/AXCL.c"),
+        Object(Matching, "dolphin/ax/AXOut.c"),
+        Object(Matching, "dolphin/ax/AXProf.c"),
+        Object(Matching, "dolphin/ax/AXSPB.c"),
+        Object(Matching, "dolphin/ax/AXVPB.c"),
+        Object(Matching, "dolphin/ax/DSPCode.c"),
+    ]),
+    DolphinLib("axfx", [
+        Object(Matching, "dolphin/axfx/chorus.c"),
+        Object(Matching, "dolphin/axfx/delay.c"),
+        Object(Matching, "dolphin/axfx/reverb_hi.c"),
+        Object(Matching, "dolphin/axfx/reverb_std.c"),
+    ]),
+    DolphinLib("base", [
+        Object(Matching, "dolphin/base/PPCArch.c"),
+        Object(Matching, "dolphin/base/PPCPm.c"),
+    ]),
+    DolphinLib("card", [
+        Object(Matching, "dolphin/card/CARDBios.c"),
+        Object(Matching, "dolphin/card/CARDBlock.c"),
+        Object(Matching, "dolphin/card/CARDCheck.c"),
+        Object(Matching, "dolphin/card/CARDCreate.c"),
+        Object(Matching, "dolphin/card/CARDDelete.c"),
+        Object(Matching, "dolphin/card/CARDDir.c"),
+        Object(Matching, "dolphin/card/CARDFormat.c"),
+        Object(Matching, "dolphin/card/CARDMount.c"),
+        Object(Matching, "dolphin/card/CARDOpen.c"),
+        Object(Matching, "dolphin/card/CARDRaw.c"),
+        Object(Matching, "dolphin/card/CARDRdwr.c"),
+        Object(Matching, "dolphin/card/CARDRead.c"),
+        Object(Matching, "dolphin/card/CARDRename.c"),
+        Object(Matching, "dolphin/card/CARDStat.c"),
+        Object(Matching, "dolphin/card/CARDStatEx.c"),
+        Object(Matching, "dolphin/card/CARDUnlock.c"),
+        Object(Matching, "dolphin/card/CARDWrite.c"),
+    ]),
+    DolphinLib("db", [
+        Object(Matching, "dolphin/db/db.c"),
+    ]),
+    DolphinLib("demo", [
+        Object(Matching, "dolphin/demo/DEMOFont.c"),
+        Object(Matching, "dolphin/demo/DEMOInit.c"),
+        Object(Matching, "dolphin/demo/DEMOPad.c"),
+        Object(Matching, "dolphin/demo/DEMOPuts.c"),
+        Object(Matching, "dolphin/demo/DEMOStats.c"),
+        Object(Matching, "dolphin/demo/DEMOWin.c"),
+    ]),
+    DolphinLib("dolformat", [
+        Object(Matching, "dolphin/dolformat/DOLLoader.c"),
+    ]),
+    DolphinLib("dsp", [
+        Object(Matching, "dolphin/dsp/dsp_debug.c"),
+        Object(Matching, "dolphin/dsp/dsp_task.c"),
+        Object(Matching, "dolphin/dsp/dsp.c"),
+    ]),
+    DolphinLib("dtk", [
+        Object(Matching, "dolphin/dtk/dtk.c"),
+    ]),
+    DolphinLib("dvd", [
+        Object(Matching, "dolphin/dvd/dvd.c"),
+        Object(Matching, "dolphin/dvd/dvdfs.c"),
+        Object(Matching, "dolphin/dvd/dvdlow.c"),
+        Object(Matching, "dolphin/dvd/dvdqueue.c"),
+        Object(Matching, "dolphin/dvd/fstload.c"),
+    ]),
+    DolphinLib("fileCache", [
+        Object(Matching, "dolphin/fileCache/fileCache.c"),
+    ]),
+    DolphinLib("G2D", [
+        Object(Matching, "dolphin/G2D/G2D.c"),
+    ]),
+    DolphinLib("gx", [
+        Object(Matching, "dolphin/gx/GXAttr.c"),
+        Object(Matching, "dolphin/gx/GXBump.c"),
+        Object(Matching, "dolphin/gx/GXDisplayList.c"),
+        Object(Matching, "dolphin/gx/GXDraw.c"),
+        Object(Matching, "dolphin/gx/GXFifo.c"),
+        Object(Matching, "dolphin/gx/GXFrameBuf.c"),
+        Object(Matching, "dolphin/gx/GXGeometry.c"),
+        Object(Matching, "dolphin/gx/GXInit.c"),
+        Object(Matching, "dolphin/gx/GXLight.c"),
+        Object(Matching, "dolphin/gx/GXMisc.c"),
+        Object(Matching, "dolphin/gx/GXPerf.c"),
+        Object(Matching, "dolphin/gx/GXPixel.c"),
+        Object(Matching, "dolphin/gx/GXSave.c"),
+        Object(Matching, "dolphin/gx/GXStubs.c"),
+        Object(Matching, "dolphin/gx/GXTev.c"),
+        Object(Matching, "dolphin/gx/GXTexture.c"),
+        Object(Matching, "dolphin/gx/GXTransform.c"),
+        Object(Matching, "dolphin/gx/GXVerifRAS.c"),
+        Object(Matching, "dolphin/gx/GXVerifXF.c"),
+        Object(Matching, "dolphin/gx/GXVerify.c"),
+        Object(Matching, "dolphin/gx/GXVert.c"),
+    ]),
+    DolphinLib("hio", [
+        Object(Matching, "dolphin/hio/hio.c"),
+    ]),
+    DolphinLib("mcc", [
+        Object(Matching, "dolphin/mcc/fio.c"),
+        Object(Matching, "dolphin/mcc/mcc.c"),
+        Object(Matching, "dolphin/mcc/tty.c"),
+    ]),
+    DolphinLib("mix", [
+        Object(Matching, "dolphin/mix/mix.c"),
+    ]),
+    DolphinLib("mtx", [
+        Object(Matching, "dolphin/mtx/mtx.c"),
+        Object(Matching, "dolphin/mtx/mtx44.c"),
+        Object(Matching, "dolphin/mtx/mtxstack.c"),
+        Object(Matching, "dolphin/mtx/mtxvec.c"),
+        Object(Matching, "dolphin/mtx/psmtx.c"),
+        Object(Matching, "dolphin/mtx/vec.c"),
+    ]),
+    DolphinLib("odemustubs", [
+        Object(Matching, "dolphin/odemustubs/odemustubs.c"),
+    ]),
+    DolphinLib("odenotstub", [
+        Object(Matching, "dolphin/odenotstub/odenotstub.c"),
+    ]),
+    DolphinLib("os", [
+        Object(Matching, "dolphin/os/OSAddress.c"),
+        Object(Matching, "dolphin/os/OSAlarm.c"),
+        Object(Matching, "dolphin/os/OSAlloc.c"),
+        Object(Matching, "dolphin/os/OSArena.c"),
+        Object(Matching, "dolphin/os/OSAudioSystem.c"),
+        Object(Matching, "dolphin/os/OS.c"),
+        Object(Matching, "dolphin/os/OSCache.c"),
+        Object(Matching, "dolphin/os/OSContext.c"),
+        Object(Matching, "dolphin/os/OSError.c"),
+        Object(Matching, "dolphin/os/OSExiAd16.c"),
+        Object(Matching, "dolphin/os/OSExi.c"),
+        Object(Matching, "dolphin/os/OSFont.c"),
+        Object(Matching, "dolphin/os/OSInterrupt.c"),
+        Object(Matching, "dolphin/os/OSLink.c"),
+        Object(Matching, "dolphin/os/OSMemory.c"),
+        Object(Matching, "dolphin/os/OSMessage.c"),
+        Object(Matching, "dolphin/os/OSMutex.c"),
+        Object(Matching, "dolphin/os/OSReset.c"),
+        Object(Matching, "dolphin/os/OSResetSW.c"),
+        Object(Matching, "dolphin/os/OSRtc.c"),
+        Object(Matching, "dolphin/os/OSSerial.c"),
+        Object(Matching, "dolphin/os/OSStopwatch.c"),
+        Object(Matching, "dolphin/os/OSSync.c"),
+        Object(Matching, "dolphin/os/OSThread.c"),
+        Object(Matching, "dolphin/os/OSTime.c"),
+        Object(Matching, "dolphin/os/OSTimer.c"),
+        Object(Matching, "dolphin/os/OSUartExi.c"),
+        Object(Matching, "dolphin/os/__ppc_eabi_init.c"),
+        Object(Matching, "dolphin/os/__start.c"),
+        Object(Matching, "dolphin/os/time.dolphin.c"),
+    ]),
+    DolphinLib("pad", [
+        Object(Matching, "dolphin/pad/Pad.c"),
+        Object(Matching, "dolphin/pad/Padclamp.c"),
+    ]),
+    DolphinLib("perf", [
+        Object(Matching, "dolphin/perf/perf.c"),
+        Object(Matching, "dolphin/perf/perfdraw.c"),
+    ]),
+    DolphinLib("seq", [
+        Object(Matching, "dolphin/seq/seq.c"),
+    ]),
+    DolphinLib("support", [
+        Object(Matching, "dolphin/support/HTable.c"),
+        Object(Matching, "dolphin/support/List.c"),
+        Object(Matching, "dolphin/support/string.c"),
+        Object(Matching, "dolphin/support/Tree.c"),
+    ]),
+    DolphinLib("syn", [
+        Object(Matching, "dolphin/support/syn.c"),
+        Object(Matching, "dolphin/support/synctrl.c"),
+        Object(Matching, "dolphin/support/synenv.c"),
+        Object(Matching, "dolphin/support/synlfo.c"),
+        Object(Matching, "dolphin/support/synmix.c"),
+        Object(Matching, "dolphin/support/synpitch.c"),
+        Object(Matching, "dolphin/support/synsample.c"),
+        Object(Matching, "dolphin/support/synvoice.c"),
+        Object(Matching, "dolphin/support/synwt.c"),
+    ]),
+    DolphinLib("texPalette", [
+        Object(Matching, "dolphin/texPalette/texPalette.c"),
+    ]),
+    DolphinLib("vi", [
+        Object(Matching, "dolphin/vi/gpioexi.c"),
+        Object(Matching, "dolphin/vi/i2c.c"),
+        Object(Matching, "dolphin/vi/initphilips.c"),
+        Object(Matching, "dolphin/vi/vi.c"),
+    ]),
+    DolphinLib("stub", [
+        Object(Matching, "dolphin/stub.c"),
+    ]),
     {
         "lib": "main",
         "mw_version": config.linker_version,
@@ -289,8 +497,16 @@ config.libs = [
             Object(Matching, "debug/dimenu.c"),
             Object(NonMatching, "debug/prof.c"),
             Object(Matching, "main/main.c"),
-            Object(NonMatching, "main/mm_dolphin.c",  extra_cflags=['-O0,p', '-use_lmw_stmw on']),
-            Object(NonMatching, "main/models_dolphin.c",  extra_cflags=[*cflags_base, '-O0,s', '-use_lmw_stmw on']),
+            Object(
+                NonMatching,
+                "main/mm_dolphin.c",
+                extra_cflags=["-O0,p", "-use_lmw_stmw on"],
+            ),
+            Object(
+                NonMatching,
+                "main/models_dolphin.c",
+                extra_cflags=[*cflags_base, "-O0,s", "-use_lmw_stmw on"],
+            ),
             Object(NonMatching, "main/objects.c"),
             Object(Matching, "main/pi.c"),
         ],
@@ -310,6 +526,7 @@ def link_order_callback(module_id: int, objects: List[str]) -> List[str]:
     if module_id == 0:  # DOL
         return objects + ["dummy.c"]
     return objects
+
 
 # Uncomment to enable the link order callback.
 # config.link_order_callback = link_order_callback
