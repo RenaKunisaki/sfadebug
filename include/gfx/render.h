0bd077898f4bfa1fa50034409b0dc121ef4c5dad
Rena Kunisaki <i.am.inuyasha@gmail.com>
2025-11-27 12:14:13 -0500

    further tidying

--- include/gfx/render.h
+++ include/gfx/render.h
@@ -142,52 +142,47 @@ Gfx* RSP_pipeSync(Gfx **gfx);
 Gfx* rspPipeSyncFn800a6900(Gfx **gfx);
 Gfx* RSP_setTevColor2(Gfx **gfx,u8 r,u8 g,u8 b,u8 a);
 
-#define RSP_CMD(gfx, op, prm)         \
-	do {                              \
-		Gfx *gfx_ = (*(gfx))++;      \
-		gfx_->words.w0 = (op);         \
-		gfx_->words.w1 = (u32)(prm); \
-	} while(0)
+#define RSP_CMD(gfx, op, prm) do { \
+    Gfx *gfx_ = (*(gfx))++; \
+    gfx_->words.w0 = (op); \
+    gfx_->words.w1 = (u32)(prm); \
+} while(0)
 
-#define RSP_CMD_NOINC(gfx, op, prm)   \
-	do {                              \
-		Gfx *gfx_ = (*gfx);          \
-		gfx_->words.w0 = (op);         \
-		gfx_->words.w1 = (u32)(prm); \
-	} while(0)
+#define RSP_CMD_NOINC(gfx, op, prm) do { \
+    Gfx *gfx_ = (*gfx); \
+    gfx_->words.w0 = (op); \
+    gfx_->words.w1 = (u32)(prm); \
+} while(0)
 
-#define RDP_SET_CIMG(gfx, op, prm) \
-    do { \
-        (gfx)->words.w0 = _SHIFTL(G_SETCIMG, 24, 8) | (op); \
-        (gfx)->words.w1 = (u32)(prm); \
-        RSP_pipeSync(&gfx); \
-    } while(0)
+#define RDP_SET_CIMG(gfx, op, prm) do { \
+    (gfx)->words.w0 = _SHIFTL(G_SETCIMG, 24, 8) | (op); \
+    (gfx)->words.w1 = (u32)(prm); \
+    RSP_pipeSync(&gfx); \
+} while(0)
 
-#define RDP_SET_OTHER_MODE(gfx, op, prm) \
-    do { \
-        Gfx *gfx_ = (gfx); \
-        (gfx_)->words.w0 = _SHIFTL(G_RDPSETOTHERMODE, 24, 8) | (op); \
-        (gfx_)->words.w1 = (u32)(prm); \
-        rspPipeSyncFn800a6900(&gfx_); \
-    } while(0)
+#define RDP_SET_OTHER_MODE(gfx, op, prm) do { \
+    Gfx *gfx_ = (gfx); \
+    (gfx_)->words.w0 = _SHIFTL(G_RDPSETOTHERMODE, 24, 8) | \
+        (op); \
+    (gfx_)->words.w1 = (u32)(prm); \
+    rspPipeSyncFn800a6900(&gfx_); \
+} while(0)
 
-#define RDP_SET_COMBINE(gfx, op, prm) \
-    do { \
-        Gfx *gfx_ = (gfx); \
-        (gfx_)->words.w0 = _SHIFTL(G_SETCOMBINE, 24, 8) | (op); \
-        (gfx_)->words.w1 = (u32)(prm); \
-        RSP_pipeSync(&gfx_); \
-    } while(0)
+#define RDP_SET_COMBINE(gfx, op, prm) do { \
+    Gfx *gfx_ = (gfx); \
+    (gfx_)->words.w0 = _SHIFTL(G_SETCOMBINE, 24, 8) | (op); \
+    (gfx_)->words.w1 = (u32)(prm); \
+    RSP_pipeSync(&gfx_); \
+} while(0)
 
-#define	RDP_SET_IMAGE(gfx, fmt, siz, width, i) \
-    do { \
-        Gfx *gfx_ = (gfx); \
-        (gfx_)->words.w0 = _SHIFTL(G_SETTIMG, 24, 8) | \
-            _SHIFTL(fmt, 21, 3) | \
-            _SHIFTL(siz, 19, 2) | \
-            _SHIFTL((width)-1, 0, 12); \
-        (gfx_)->words.w1 = (unsigned int)(i);				\
-    } while(0)
+#define	RDP_SET_IMAGE(gfx, fmt, siz, width, i) do { \
+    Gfx *gfx_ = (gfx); \
+    (gfx_)->words.w0 = _SHIFTL(G_SETTIMG, 24, 8) | \
+        _SHIFTL(fmt, 21, 3) | \
+        _SHIFTL(siz, 19, 2) | \
+        _SHIFTL((width)-1, 0, 12); \
+    (gfx_)->words.w1 = (unsigned int)(i); \
+} while(0)
 
 //this is one of the game's custom commands.
 //it expects the next two commands to follow it in this
