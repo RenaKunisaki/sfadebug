85d5e85baee23c231899ee64bc5fc63650a233f0
Rena Kunisaki <i.am.inuyasha@gmail.com>
2025-12-12 20:18:26 -0500

    piRomLoadSection: 98.55%

--- src/main/pi_dolphin.c
+++ src/main/pi_dolphin.c
@@ -23,10 +23,6 @@
 #include <stddef.h>
 #include "placeholder.h"
 
-int lzoDecompress(void *src,int compLen,void *dest,int *outLen);
-
-void *dataFilePtrs[NUM_FILES];
-
 #define SIG_UNCOMPRESSED_FILE 0xe0e0e0e0
 #define SIG_LZO_COMPRESSED_FILE 0xf0f0f0f0
 typedef struct {
@@ -38,28 +34,109 @@ typedef struct {
     /* 0x14 */ int unk14;
 } DbMapsBinEntry;
 
+int lzoDecompress(void *src,int compLen,void *dest,int *outLen);
+
+const char *dataFileNames[] = { //802ef02c
+    "AUDIO.tab", "AUDIO.bin",
+    "SFX.tab", "SFX.bin",
+    "AMBIENT.tab", "AMBIENT.bin",
+    "MUSIC.tab", "MUSIC.bin",
+    "MPEG.tab", "MPEG.bin",
+    "MUSICACT.bin",
+    "CAMACTIO.bin",
+    "LACTIONS.bin",
+    "ANIMCURV.bin", "ANIMCURV.tab",
+    "OBJSEQ2C.tab",
+    "FONTS.bin",
+    "CACHEFON.bin", "CACHEFON.bin",
+    "GAMETEXT.bin", "GAMETEXT.tab",
+    "globalma.bin",
+    "TABLES.bin", "TABLES.tab",
+    "SCREENS.bin", "SCREENS.tab",
+    "VOXMAP.tab", "VOXMAP.bin",
+    "WARPTAB.bin",
+    "MAPS.bin", "MAPS.tab",
+    "MAPINFO.bin",
+    "TEX1.bin", "TEX1.tab",
+    "TEXTABLE.bin",
+    "TEX0.bin", "TEX0.tab",
+    "BLOCKS.bin", "BLOCKS.tab",
+    "TRKBLK.tab",
+    "HITS.bin", "HITS.tab",
+    "MODELS.tab", "MODELS.bin",
+    "MODELIND.bin",
+    "MODANIM.TAB", "MODANIM.BIN",
+    "ANIM.TAB", "ANIM.BIN",
+    "AMAP.TAB", "AMAP.BIN",
+    "BITTABLE.bin",
+    "WEAPONDA.bin",
+    "VOXOBJ.tab", "VOXOBJ.bin",
+    "MODLINES.bin", "MODLINES.tab",
+    "SAVEGAME.bin", "SAVEGAME.tab",
+    "OBJSEQ.bin", "OBJSEQ.tab",
+    "OBJECTS.tab", "OBJECTS.bin",
+    "OBJINDEX.bin",
+    "OBJEVENT.bin",
+    "OBJHITS.bin",
+    "DLLS.bin", "DLLS.tab",
+    "DLLSIMPO.bin",
+    "MODELS.tab", "MODELS.bin",
+    "BLOCKS.bin", "BLOCKS.tab",
+    "ANIM.TAB", "ANIM.BIN",
+    "TEX1.bin", "TEX1.tab",
+    "TEX0.bin", "TEX0.tab",
+    "ENVFXACT.bin",
+    "NULL", "NULL"};
+
+const char *mapDirNames[] = {
+    "animtest",
+    "dragrock",
+    "dragrockbot",
+    "swapholbot",
+    "wallcity",
+    "lightfoot",
+    "cloudtreasure",
+    "clouddungeon",
+    "darkicemines",
+    "icemountain",
+    "darkicemines2",
+    "bossgaldon",
+    "insidegal",
+    "magiccave",
+    "dfshrine",
+    "mmshrine",
+    "ecshrine",
+    "gpshrine",
+    "dbshrine",
+    "nwshrine",
+    "worldmap",
+    "capeclaw",
+    "cloudrace",
+    "bossdrakor",
+    "bosstrex"};
+
+void *dataFilePtrs[NUM_FILES];
+
 void piRomLoadSection(int id, void *dest) {
 	int offset;
-	DbMapsBinEntry *mapsBin;
 	DbMapsBinEntry *entry;
+    uint src;
 	int len;
     int outLen;
+    u8 _pad[8];
 
 	if(!dataFilePtrs[FILE_MAPS_bin]) {
 		OSPanic("pi_dolphin.c", 1084,
             "piRomLoadSection(): DB_MAPS Bin Not Loaded");
 	}
-    mapsBin = dataFilePtrs[FILE_MAPS_bin];
-	entry = (DbMapsBinEntry *)((int)mapsBin + id);
+    entry = (DbMapsBinEntry *)((int)dataFilePtrs[FILE_MAPS_bin] + id);
 	if(entry->sig == SIG_UNCOMPRESSED_FILE) {
-        memcpy_src_dst_len(
-            //what the hell is going on here
-            (void *)(
-                (uint)mapsBin + (
-                    (entry->offset + sizeof(DbMapsBinEntry) + (uint)entry) -
-                    (uint)dataFilePtrs[FILE_MAPS_bin]
-                ) + id
-            ),
+        //what the hell is going on here
+        src = (
+            (entry->offset + sizeof(DbMapsBinEntry) + (uint)id) -
+            (uint)dataFilePtrs[FILE_MAPS_bin]
+        );
+        memcpy_src_dst_len((void*)((uint)src + (uint)dataFilePtrs[FILE_MAPS_bin]),
             dest,entry->len);
 	} else if(entry->sig == SIG_LZO_COMPRESSED_FILE) { //LZO compressed file.
 		offset = (entry->offset + 0x28) + (int)entry - (int)dataFilePtrs[FILE_MAPS_bin];
@@ -72,7 +149,7 @@ void piRomLoadSection(int id, void *dest) {
 
         PPCMtmmcr0(0);
 		PPCMtmmcr1(0);
-        STUBBED_PRINTF("", //TODO: check for matching format strings
+        STUBBED_PRINTF("<Cyc %d | Instr %d | L1misscyc %d | DCmiss %d>\n",
             (int)(PPCMfpmc1() * 0.001f),
             (int)(PPCMfpmc2() * 0.001f),
             (int)(PPCMfpmc3() * 0.001f),
@@ -84,5 +161,4 @@ void piRomLoadSection(int id, void *dest) {
 
 		DCStoreRange(dest, len);
 	}
-	return;
 }
