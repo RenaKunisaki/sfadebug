d157de7aeb5a899266c60f6dc36c1b1f55c31e6b
Rena Kunisaki <i.am.inuyasha@gmail.com>
2026-02-22 19:56:28 -0500

    mark objlist.c matching

--- configure.py
+++ configure.py
@@ -504,7 +504,7 @@ config.libs = [
             Object(Matching, "main/models_dolphin.c",
                 extra_cflags=[*cflags_base, "-O0,s", "-use_lmw_stmw on"],
             ),
-            Object(NonMatching, "main/objlist.c"),
+            Object(Matching, "main/objlist.c"),
             Object(Matching, "main/objects.c"),
             Object(NonMatching, "main/objprint.c"),
             Object(Matching, "main/pi.c"),
