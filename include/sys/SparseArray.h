29914aaac01694d00a44914ef39b1cc8d3a83074
Rena Kunisaki <i.am.inuyasha@gmail.com>
2026-03-23 19:43:06 -0400

    match SparseArray_remove

--- include/sys/SparseArray.h
+++ include/sys/SparseArray.h
@@ -9,7 +9,7 @@ typedef struct {
 
 typedef struct {
     /* 0x00 */ SparseArrayItem *items;
-    /* 0x04 */ SparseArrayItem *lastElem;
+    /* 0x04 */ SparseArrayItem *highPtr;
     /* 0x08 */ SparseArrayItem *endPtr;
     /* 0x0c */ u8 keySize;
     /* 0x0d */ u8 elemSize;
--- src/main/sparsearray.c
+++ src/main/sparsearray.c
@@ -14,7 +14,7 @@ SparseArray *SparseArray_create(int capacity, int itemSize) {
 	sparseArray->items = (SparseArrayItem *)((int)sparseArray + sizeof(SparseArray));
 	sparseArray->keySize = itemSize;
 	sparseArray->elemSize = (itemSize + 2U) >> 1;
-	sparseArray->lastElem = (SparseArrayItem*)&sparseArray->items->key;
+	sparseArray->highPtr = (SparseArrayItem*)&sparseArray->items->key;
 	sparseArray->endPtr = (SparseArrayItem*)&sparseArray->items[
         capacity * sparseArray->elemSize].key;
 	memset_(sparseArray->items, -1,
@@ -28,14 +28,34 @@ void SparseArray_set(SparseArray *sparseArray, short key, void *newElement) {
     ASSERTLINE(95, sparseArray);
     ASSERTLINE(96, newElement);
 	for(keyPtr = sparseArray->items;
-	    keyPtr < (SparseArrayItem *)sparseArray->lastElem;
+	    keyPtr < (SparseArrayItem *)sparseArray->highPtr;
 	    keyPtr += sparseArray->elemSize) {
             if(keyPtr->key == -1) break;
         }
     ASSERTLINE(110, keyPtr<sparseArray->endPtr);
 	keyPtr->key = key;
 	memcpy(keyPtr + 1, newElement, (uint)sparseArray->keySize);
-	if(keyPtr == (SparseArrayItem *)sparseArray->lastElem) {
-		sparseArray->lastElem = sparseArray->lastElem + sparseArray->elemSize;
+	if(keyPtr == (SparseArrayItem *)sparseArray->highPtr) {
+		sparseArray->highPtr = sparseArray->highPtr + sparseArray->elemSize;
 	}
+}
+
+void SparseArray_remove(SparseArray *sparseArray, int key) {
+	SparseArrayItem *keyPtr;
+
+    ASSERTLINE(144, sparseArray);
+	for(keyPtr = sparseArray->items;
+    keyPtr < sparseArray->highPtr;
+    keyPtr += sparseArray->elemSize) {
+		if(keyPtr->key == key) {
+            keyPtr->key = -1;
+            break;
+        }
+	}
+
+    ASSERTLINE(157, keyPtr!=sparseArray->highPtr);
+    while((sparseArray->highPtr > sparseArray->items
+    &&    (sparseArray->highPtr[-1].key == -1))) {
+        sparseArray->highPtr -= sparseArray->elemSize;
+    }
 }
